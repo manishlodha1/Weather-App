436132a639094ec3d08da5b9a5862e73f0de97f5
ManishLodha1 <108422478+manishlodha1@users.noreply.github.com>
2023-11-02 03:09:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import WeatherApp from './Components/WeatherApp/WeatherApp';
+// import Error from './Components/WeatherApp/Error';
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp />
+    </div>
+  );
+}
+
+export default App;
